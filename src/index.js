39872c61a1736fc67ea833a8a3877ef521093f39
theflyingpinata <42786585+theflyingpinata@users.noreply.github.com>
2021-02-15 17:00:34 -0800

    Phase 1

--- src/index.js
+++ src/index.js
@@ -2,54 +2,20 @@ console.log('First web service starting up ...');
 
 // 1 - pull in the HTTP server module
 const http = require('http');
+const url = require('url');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
 // 2 - pull in URL and query modules (for URL parsing)
-const url = require('url');
 
 // 3 - locally this will be 3000, on Heroku it will be assigned
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-// 5 - here's our 404 page
-const errorPage = `
-<html>
-    <head>
-        <title>404 - File Not Found</title>
-    </head>
-    <body>
-        <h1>404 - File Not Found</h1>
-        <p>Check your URL, or your typing!!</p>
-        <p>:-)</p>
-    </body>
-</html>`;
 
-const jokes = [
-  { q: 'What do you call a very small valentine?', a: 'A valen-tiny!!!' },
-  { q: 'What did the dog say when he rubbed his tail on the sandpaper?', a: 'Ruff, Ruff!!!' },
-  { q: "Why don't sharks like to eat clowns?", a: 'Because they taste funny!!!' },
-  { q: 'What did the boy cat say to the girl cat?', a: "You're Purr-fect!!!" },
-  { q: "What is a frog's favorite outdoor sport?", a: 'Fly Fishing!!!' },
-  { q: 'I hate jokes about German sausages.', a: 'Theyre the wurst!!!' },
-  { q: 'Did you hear about the cheese factory that exploded in France?', a: 'There was nothing left but de Brie!!!' },
-  { q: 'Our wedding was so beautiful ', a: 'Even the cake was in tiers!!!' },
-  { q: 'Is this pool safe for diving?', a: 'It deep ends!!!' },
-  { q: 'Dad, can you put my shoes on?', a: 'I dont think theyll fit me!!!' },
-  { q: 'Can February March?', a: 'No, but April May!!!' },
-  { q: 'What lies at the bottom of the ocean and twitches?', a: 'A nervous wreck!!!' },
-  { q: 'Im reading a book on the history of glue.', a: 'I just cant seem to put it down!!!' },
-  { q: 'Dad, can you put the cat out?', a: 'I didnt know it was on fire!!!' },
-  { q: 'What did the ocean say to the sailboat?', a: 'Nothing, it just waved!!!' },
-  { q: 'What do you get when you cross a snowman with a vampire?', a: 'Frostbite!!!!!' },
-];
-const getRandomJoke = () => {
-  const numJokes = jokes.length;
-  const randomNum = parseInt(Math.random() * numJokes, 10);
-  const randomJoke = jokes[randomNum];
-  const responseObj = {
-    q: randomJoke.q,
-    a: randomJoke.a,
-  };
-  return JSON.stringify(responseObj);
-};
+const urlStruct = {
+  '/random-joke': jsonHandler.getRandomJokeResponse,
+  notFound: htmlHandler.get404Response
+}
 
 // 7 - this is the function that will be called every time a client request comes in
 // this time we will look at the `pathname`, and send back the appropriate page
@@ -61,14 +27,10 @@ const onRequest = (request, response) => {
   // console.log("parsedUrl=", parsedUrl);
   // console.log("pathname=", pathname);
 
-  if (pathname === '/random-joke') {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.write(getRandomJoke());
-    response.end();
+  if (urlStruct[pathname]) {
+    urlStruct[pathname](request, response);
   } else {
-    response.writeHead(404, { 'Content-Type': 'text/html' });
-    response.write(errorPage);
-    response.end();
+    urlStruct.notFound(request, response);
   }
 };
 
